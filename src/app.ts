db96eea78aa50fb69f11723fbf72195a35606b5a
ofktuhin <tuhin.omar99@gmail.com>
2023-05-30 07:26:15 +0600

    [Done] setup cors express mongoose

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import cors from "cors";
+import express, { Application } from "express";
+const app: Application = express();
+// middle ware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
