18387132c3255fc4fc806d81977fcd2179ea9810
ofktuhin <tuhin.omar99@gmail.com>
2023-06-07 08:55:47 +0600

    complete add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,12 @@
+/* eslint-disable no-undef */
 import dotenv from 'dotenv'
 import path from 'path'
 
+// eslint-disable-next-line no-undef
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
+  // eslint-disable-next-line no-undef
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_user_password: process.env.DEFAULT_USER_PASSWORD,
