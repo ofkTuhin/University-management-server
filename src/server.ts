ba2ab5bd6ae631900c6101bff01425da7e226928
ofktuhin <tuhin.omar99@gmail.com>
2023-06-07 08:55:28 +0600

    complete add logger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './share/logger'
 const dbConnect = async () => {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('database connect successfully')
+    logger.successlog.info('database connect successfully')
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.successlog.info(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('error occured in db connection')
+    logger.errorlog.error('error occured in db connection')
   }
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
---
+++ src/share/logger.ts
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
+const { combine, timestamp, label, printf, prettyPrint } = format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const min = date.getMinutes()
+  const sec = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${min}:${sec}  [${label}] ${level}: ${message}`
+})
+const successlog = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    // eslint-disable-next-line no-undef
+
+    new DailyRotateFile({
+      filename: path.join(process.cwd(), 'logs', 'ums-success-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new transports.Console(),
+  ],
+})
+
+const errorlog = createLogger({
+  level: 'error',
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    // eslint-disable-next-line no-undef
+
+    new DailyRotateFile({
+      filename: path.join(process.cwd(), 'logs', 'ums-error-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new transports.Console(),
+  ],
+})
+export default {
+  successlog,
+  errorlog,
+}
